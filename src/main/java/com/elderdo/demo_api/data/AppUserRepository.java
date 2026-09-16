ad7c5bb644f052b98001472e857485899900db0d
elderdo <elderdo@yahoo.com>
2026-09-16 13:24:10 -0500

    removed unnecessary @Repository annotation.  added the spring boot starter data jpa

--- src/main/java/com/elderdo/demo_api/data/AppUserRepository.java
+++ src/main/java/com/elderdo/demo_api/data/AppUserRepository.java
@@ -2,9 +2,7 @@ package com.elderdo.demo_api.data;
 
 import com.elderdo.demo_api.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
     // Inherits .findAll() and .findById() hooks instantly using Spring Dependency Injection
 }
